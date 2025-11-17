7bcff24ffdadcfdd2103146081a5eb18e2a96d2a
Rakhib-Mohammad <rakhibmohammad17@gmail.com>
2025-11-17 19:41:15 +0530

    test

--- test_app.py
+++ test_app.py
@@ -2,6 +2,7 @@ from app import app
 
 def test_home():
     response=app.test_client().get("/")
+    ### Check that the response status code is 200 and the response data is "Hello World!"
 
     assert response.status_code==200
     assert response.data== b"Hello World!"
