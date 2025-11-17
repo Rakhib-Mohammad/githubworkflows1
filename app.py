0e5a03b380b6effa46baab303605029cef6c6067
Rakhib-Mohammad <rakhibmohammad17@gmail.com>
2025-11-17 19:32:02 +0530

    Latest codes in flask

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app=Flask(__name__)
 
+## A simple route that returns "Hello World!"
+
 @app.route("/")
 def home():
-    return "Hello World!"
+    return "Hello World! App is running."
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000)
